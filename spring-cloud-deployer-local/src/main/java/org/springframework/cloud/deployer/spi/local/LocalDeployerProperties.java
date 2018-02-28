89b59b7f65db7762b5cda6133c5617c88c15768b
Janne Valkealahti <janne.valkealahti@gmail.com>
2018-02-28 18:52:29 +0000

    stash2

--- spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties.java
+++ spring-cloud-deployer-local/src/main/java/org/springframework/cloud/deployer/spi/local/LocalDeployerProperties.java
@@ -71,6 +71,12 @@ public class LocalDeployerProperties {
 
 	private static final String JAVA_COMMAND = LocalDeployerUtils.isWindows() ? "java.exe" : "java";
 
+	private static final String[] ENV_VARS_TO_INHERIT_DEFAULTS_WIN = { "TMP", "TEMP", "Path",
+			AbstractLocalDeployerSupport.SPRING_APPLICATION_JSON };
+
+	private static final String[] ENV_VARS_TO_INHERIT_DEFAULTS_OTHER = { "TMP", "LANG", "LANGUAGE", "LC_.*", "PATH",
+			AbstractLocalDeployerSupport.SPRING_APPLICATION_JSON };
+
 	/**
 	 * Directory in which all created processes will run and create log files.
 	 */
@@ -85,7 +91,8 @@ public class LocalDeployerProperties {
 	 * Array of regular expression patterns for environment variables that
 	 * should be passed to launched applications.
 	 */
-	private String[] envVarsToInherit = {"TMP", "LANG", "LANGUAGE", "LC_.*", "PATH", AbstractLocalDeployerSupport.SPRING_APPLICATION_JSON};
+	private String[] envVarsToInherit = LocalDeployerUtils.isWindows() ? ENV_VARS_TO_INHERIT_DEFAULTS_WIN
+			: ENV_VARS_TO_INHERIT_DEFAULTS_OTHER;
 
 	/**
 	 * The command to run java.
